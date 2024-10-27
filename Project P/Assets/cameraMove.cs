876aff5c820a42f3e6f946e9409cd980bc88cccf
everett-alanHood <everettalan.hood@gmail.com>
2024-10-27 13:47:08 -0400

    Add Camera & Ground Check Script

---
+++ Project P/Assets/cameraMove.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    float xRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float MouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float MouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        xRotation -= MouseY;
+        xRotation = Math.Clamp(xRotation, -89f, 89f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * MouseX); 
+    }
+}
