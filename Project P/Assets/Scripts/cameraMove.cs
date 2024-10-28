3f6518a2ff58cc7d87e977932b764f017e77b81b
everett-alanHood <everettalan.hood@gmail.com>
2024-10-28 10:51:33 -0400

    Increase Mouse Speed

--- Project P/Assets/Scripts/cameraMove.cs
+++ Project P/Assets/Scripts/cameraMove.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class cameraMove : MonoBehaviour
 {
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 400f;
     public Transform playerBody;
     float xRotation = 0f;
     bool canFreeMouse = false;
