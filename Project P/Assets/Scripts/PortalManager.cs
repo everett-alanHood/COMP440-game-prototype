e963a149e929c14396ba05ace1d91c2c0734b900
everett-alanHood <everettalan.hood@gmail.com>
2024-11-15 14:30:35 -0500

    Added SFX for Portals

--- Project P/Assets/Scripts/PortalManager.cs
+++ Project P/Assets/Scripts/PortalManager.cs
@@ -6,11 +6,16 @@ public class portalManager : MonoBehaviour
 {
     public Transform positionA;
     public Transform positionB;
+    public AudioSource audio_src;
+    public AudioClip teleportIN, teleportOUT;
 
     private void OnTriggerEnter(Collider col)
     {
         if (col.CompareTag("Portal A")){
             CharacterController cc = GetComponent<CharacterController>();
+
+            audio_src.clip = teleportIN;
+            audio_src.Play();
 
             cc.enabled = false;
             transform.position = positionB.transform.position;
@@ -21,6 +26,9 @@ public class portalManager : MonoBehaviour
 
         if (col.CompareTag("Portal B")){
             CharacterController cc = GetComponent<CharacterController>();
+            
+            audio_src.clip = teleportOUT;
+            audio_src.Play();
             
             cc.enabled = false;
             transform.position = positionA.transform.position;
