41aab2ac01578f04cd8b859c06edeb00c173215e
everett-alanHood <everettalan.hood@gmail.com>
2024-11-15 07:57:51 -0500

    Added PowerWash Script

---
+++ Project P/Assets/Scripts/PowerWash.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PowerWash : MonoBehaviour
+{
+    public UnityEvent onShoot;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButton(1)){
+            onShoot.Invoke();
+        }
+    }
+}
