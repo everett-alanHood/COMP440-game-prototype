6424688c55c0316172e2c79215e1b8b246b500ba
everett-alanHood <everettalan.hood@gmail.com>
2024-11-15 12:12:10 -0500

    Added Power Washer Gun

--- Project P/Assets/Scripts/Entity.cs
+++ Project P/Assets/Scripts/Entity.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // This class acts as a tag or marker for portal objects
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Entity script attached to: " + gameObject.name);
     }
 }
